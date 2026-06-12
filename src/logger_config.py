4a60bb103551ec11d9f2de3f8ef2dd534c68bfa8
eriklegramante-dev <eriklegramante@gmail.com>
2026-06-12 14:29:55 -0300

    update logger, to prevent error streamlit cloud

--- src/logger_config.py
+++ src/logger_config.py
@@ -1,12 +1,16 @@
 import logging
+import os
 
+if not os.path.exists("logs"):
+    os.makedirs("logs")
 
 logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s [%(levelname)s] %(message)s",
     handlers=[
-        logging.FileHandler("logs/app.log", encoding="utf-8"), 
-        logging.StreamHandler()                           
+        logging.FileHandler("logs/app.log", encoding="utf-8"),
+        logging.StreamHandler()
     ]
 )
+
 logger = logging.getLogger(__name__)
